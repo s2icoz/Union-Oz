2fc4835ce5713ea447bcaf2709c1d92bfd2e7404
zGack <smf-mena@hotmail.com>
2019-04-12 20:24:55 -0500

    added test.cpp

--- test.cpp
+++ test.cpp
@@ -3,8 +3,14 @@
 using namespace std;
 
 int main(){
+  int n1,n2;
 
   std::cout << "Hello worldsito" << '\n';
+
+  std::cin >> n1;
+  std::cin >> n2;
+
+  std::cout << "EL resultado es: "<< n1+n2 << '\n';
 
   return 0;
 }
