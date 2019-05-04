81e8612763a1022c9a80ecf247bdfb43006846e0
s2icoz <simonz@javerianacali.edu.co>
2019-05-04 00:12:02 -0500

    1) Se cambian los atributos de tipo vector a tipo lista.
    2) Por consiguiente se modifican ligermente las funciones que 
    involucraban el manejo de dichos atributos, el cabmio es ligero pues 
    todo se hace con STL.
    
    3) Se agregan comentarios formales de TAD a todas las funciones

--- ValorOz.h
+++ ValorOz.h
@@ -1,13 +1,15 @@
 #ifndef VALOROZ_H
 #define VALOROZ_H
-#include <vector>
+#include <list>
+
+using namespace std;
 
 class ValorOz{
   private:
     int val; //Un registro con campos no puede tener valor
-    std::vector<ValorOz *> campos;
+    list<ValorOz *> campos;
     ValorOz *father;  /*Una variable solo puede tener un padre*/
-    std::vector<ValorOz *> sons;  /*Una variable solo puede tener multiples hijos+
+    list<ValorOz *> sons;  /*Una variable solo puede tener multiples hijos+
                                 en caso de ser una etiqueta*/
   public:
     ValorOz(){
@@ -18,16 +20,33 @@ class ValorOz{
     }
     int get_val();
     ValorOz* get_father();
-    std::vector<ValorOz *> get_sons();
+    list<ValorOz *> get_sons();
 
     void set_father(ValorOz *n_father);
     void add_sons(ValorOz *n_son);
-    void add_sons(std::vector<ValorOz *> n_sons);
+    void add_sons(list<ValorOz *> n_sons);
     void elim_sons();
 
     void operator =(char v);
     void operator =(int num);
     void operator =(ValorOz &val2);
+
+    /*Pendiendtes*/
+    /*Obtener cadena valor, obtener etiqueta, obtener lista de campos*/
+};
+
+
+class Almacen{
+  private:
+    list<ValorOz *> variables;
+  public:
+    Almacen();/*crear almacen*/
+    void imprimir_almacen();
+    int consultar_variable(string variable);
+    bool consultar__ligadura_variable(string variable);
+    void agregar_variable(string variable);
+    list<ValorOz *> obtener_lista_variables();
+    int unificar_variables(string val1, string val2);
 };
 
 #endif
