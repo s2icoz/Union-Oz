308ae2747f96b46230fc0cb5820175907a4b2343
s2icoz <simonz@javerianacali.edu.co>
2019-05-05 23:11:45 -0500

    Se agrega el atributo etiqueta, se cambia el atributo campos a un mapa.
    1) El nuevo atributo etiqueta es un string y contempla las operaciones 
    de set y get básicas.
    
    2) El atributo campos pasa de ser una lista de punteros a ValorOz a un 
    mapa que contiene parejas con llaves string y valores *ValorOz.
    
    3) Se agregan las funciones de setear (agregar) campos, eliminar campos, 
    analizar las llaves de los campos, analizar el valor de un campo en 
    especifico.
    
    4) Se reorganizan las funciones para que sigan el orden de 
    constructoras->modificadoras->analizadoras

--- ValorOz.cpp
+++ ValorOz.cpp
@@ -29,150 +29,6 @@ int prev_union(ValorOz *objetivo, ValorOz *contenedor){
 }
 
 
-int ValorOz::get_val(){
-/*Retorna el valor de un ValorOz*/
-/*Pre: True*/
-/*Pos: -1 si Val == Vacio
-        Val si Val != Vacio*/
-  if (father != NULL){
-    return father->val;
-  }
-  else{
-    return val;
-  }
-}
-
-
-ValorOz* ValorOz::get_father(){
-/*Retorna el puntero father del ValorOz*/
-/*Pre: True*/
-/*Pos: NULL si father == NULL
-        *ValorOz si father != NULL*/
-  return father;
-}
-
-
-list<ValorOz *> ValorOz::get_sons(){
-/*Retorna la lista de hijos del ValorOz*/
-  return sons;
-}
-
-
-void ValorOz::operator =(char v){
-/*Da el valor de vacio al ValorOz*/
-/*Pre: El valor no debe tener un valor asignado, la convencion de vacio
-      debe ser '_'*/
-/*Pos: ...*/
-  if (val == -1){
-    if (v != '_'){
-      fprintf(stderr, "Asignación desconocida\n");
-      exit(1);
-    }
-  }
-  else{
-    fprintf(stderr, "Asignación a un valor no vacío\n");
-    exit(1);
-  }
-}
-
-
-void ValorOz::operator =(int num){
-/*Liga el ValorOz a un entero*/
-/*Pre: El val de ValorOz debe ser el mismo parámetro o vacio*/
-/*Pos: val == num*/
-  if (get_val() == num);
-  else if (val == -1){
-    if (father != NULL){
-      *father = num;
-    }
-    else{
-      val = num;
-    }
-  }
-  else{
-    fprintf(stderr, "Asignación de una constante a un valor con referencia\n");
-    exit(1);
-    /*referencia => que apunta a alguien,
-    /*referenciado => que alguien apunta a él.*/
-  }
-}
-
-
-void ValorOz::operator =(ValorOz &val2){
-/*Liga el ValorOz con otro ValorOz*/
-/*Pre: No pueden ambos ValorOz estar previamente ligados a otros ValorOz
-      que no sean el otro ValorOz de la operación; a no ser que compartan un
-      val diferente a vacio. Además para registros, deben tener en común:
-      etiquetas, número de campos y campos*/
-/*Pos: ValorOz ligado a Val2*/
-  if (this == &val2 || get_val() == val2.get_val() && get_val() != -1){
-    /*Caso en el que se trate de ligar un ValorOz a si mismo, o a alguien
-      ligado a él, o se iguale a un ValorOz con quien comparta val y que este
-      sea diferente a vacio*/
-    //fprintf(stderr, "Unión de una variable a si misma\n");
-  }
-  else if (campos.size() > 0 && val2.campos.size() > 0 &&
-      campos.size() == val2.campos.size()){
-    /*Caso etiquetas*/
-    /*Mas de un campo e igual cantidad de campos*/
-  }
-  else if (campos.size() + val2.campos.size() == 0){
-    ValorOz *temp_father;
-    //Comprueba si ya están ligados entre si
-    if (!prev_union(this, &val2) && !prev_union(&val2, this)){
-      //Emparentamiento de izquierda a derecha
-      if (father == NULL){
-        /*Si el otro tiene father se le pasaran los hijos del ValorOz al
-          father de val2, de lo contrario se pasaran a val2*/
-        val2.get_father()?temp_father = val2.get_father(): temp_father = &val2;
-        if (get_val() != -1){
-          *(temp_father) = get_val();
-        }
-        else if (val2.get_val() != -1)
-          *(temp_father) = val2.get_val();
-
-        temp_father->add_sons(this);
-        if (sons.size()){
-          temp_father->add_sons(get_sons());
-          elim_sons();
-        }
-        /*Si val2 tiene father el ValorOz se liga al father de val2, de lo
-          contrario se ligara a val2*/
-        set_father(temp_father);
-      }
-      //Emparentamiento de derecha a izquierda
-      else if (val2.father == NULL){
-        /*Si el ValorOz tiene father se le pasaran los hijos de val2 al
-          father, de lo contrario se pasaran al ValorOz*/
-        get_father()?temp_father = get_father(): temp_father = this;
-        if (get_val() != -1)
-          *(temp_father) = get_val();
-        else if (val2.get_val() != -1)
-          *(temp_father) = val2.get_val();
-
-        temp_father->add_sons(&val2);
-        if (val2.sons.size()){
-          temp_father->add_sons(val2.get_sons());
-          val2.elim_sons();
-        }
-        /*Si el ValorOz tiene father val2 se liga al father del ValorOz, de lo
-          contrario se ligara a ValorOz*/
-        val2.set_father(temp_father);
-      }
-      else{
-        //Casos de error
-        fprintf(stderr, "Asignacion entre variables previamente ligadas\n");
-      }
-    }
-  }
-  else{
-    if (campos.size() != val2.campos.size()){
-      fprintf(stderr, "Las etiquetas tuvieron diferentes cantidades de campos\n");
-      exit(1);
-    }
-  }
-}
-
 void ValorOz::set_father(ValorOz *n_father){
 /*Se le da un valor al father del ValorOz*/
 /*Pre: n_father != NULL*/
@@ -213,5 +69,246 @@ void ValorOz::elim_sons(){
 /*Pos: sons = <>*/
   if (!sons.empty()){
     sons.clear();
+  }
+}
+
+
+void ValorOz::set_campo(string llave, ValorOz *n_ValorOz){
+  if (etiqueta != ""){
+    campos.insert(cam(llave, n_ValorOz));
+  }
+  else{
+    fprintf(stderr, "Operacion de registro en una variable simple\n");
+    exit(1);
+  }
+}
+
+
+void ValorOz::elim_campo(string llave){
+  if (etiqueta != ""){
+    bool find = false;
+    int i = 0;
+    for (list<string>::iterator it = get_campos_llaves().begin(); i < get_campos_llaves().size(); i++, it++) {
+      if (*it == llave){
+        find = true;
+      }
+    }
+    if (find){
+      campos.erase(llave);
+    }
+    else{
+      fprintf(stderr, "Eliminacion de un campo inexistente en el ValorOz\n");
+    }
+  }
+  else{
+    fprintf(stderr, "Operacion de registro en una variable simple\n");
+    exit(1);
+  }
+}
+
+
+int ValorOz::get_val(){
+/*Retorna el valor de un ValorOz*/
+/*Pre: True*/
+/*Pos: -1 si Val == Vacio
+        Val si Val != Vacio*/
+  if (father != NULL){
+    return father->val;
+  }
+  else{
+    return val;
+  }
+}
+
+
+string ValorOz::get_etiqueta(){
+/*Retorna la etiqueta de un ValorOz*/
+/*Pre: etiqueta != vacio*/
+/*Pos: "" si etiqueta == Vacio
+        etiqueta si etiqueta != Vacio*/
+  if (etiqueta != ""){
+    return etiqueta;
+  }
+  else{
+    return "";
+  }
+}
+
+
+ValorOz* ValorOz::get_father(){
+/*Retorna el puntero father del ValorOz*/
+/*Pre: True*/
+/*Pos: NULL si father == NULL
+        *ValorOz si father != NULL*/
+  return father;
+}
+
+
+list<ValorOz *> ValorOz::get_sons(){
+/*Retorna la lista de hijos del ValorOz*/
+  return sons;
+}
+
+
+list<string> ValorOz::get_campos_llaves(){
+  list<string> llaves;
+  for (map<string, ValorOz *>::iterator it = campos.begin(); it != campos.end();
+   it++) {
+    llaves.push_back(it->first);
+  }
+  return llaves;
+}
+
+
+ValorOz* ValorOz::get_campo_valor(string llave){
+  if (etiqueta != ""){
+    bool find = false;
+    int i = 0;
+    for (list<string>::iterator it = get_campos_llaves().begin(); i < get_campos_llaves().size(); i++, it++) {
+      if (*it == llave){
+        return campos[llave];
+      }
+    }
+    fprintf(stderr, "Eliminacion de un campo inexistente en el ValorOz\n");
+  }
+  else{
+    fprintf(stderr, "Operacion de registro en una variable simple\n");
+    exit(1);
+  }
+}
+
+
+void ValorOz::operator =(char v){
+  /*Da el valor de vacio al ValorOz*/
+  /*Pre: El valor no debe tener un valor asignado, la convencion de vacio
+  debe ser '_'*/
+  /*Pos: ...*/
+  if (val == -1){
+    if (v != '_'){
+      fprintf(stderr, "Asignación desconocida\n");
+      exit(1);
+    }
+  }
+  else{
+    fprintf(stderr, "Asignación a un valor no vacío\n");
+    exit(1);
+  }
+}
+
+
+void ValorOz::operator =(int num){
+  /*Liga el ValorOz a un entero*/
+  /*Pre: El val de ValorOz debe ser el mismo parámetro o vacio*/
+  /*Pos: val == num*/
+  if (etiqueta == ""){
+    if (get_val() == num);
+    else if (val == -1){
+      if (father != NULL){
+        *father = num;
+      }
+      else{
+        val = num;
+      }
+    }
+    else{
+      fprintf(stderr, "Asignación de una constante a un valor con referencia\n");
+      exit(1);
+      /*referencia => que apunta a alguien,
+      /*referenciado => que alguien apunta a él.*/
+    }
+  }
+  else{
+    fprintf(stderr, "Asignación de una constante a un registro\n");
+    exit(1);
+  }
+}
+
+
+void ValorOz::operator =(ValorOz &val2){
+  /*Liga el ValorOz con otro ValorOz*/
+  /*Pre: No pueden ambos ValorOz estar previamente ligados a otros ValorOz
+  que no sean el otro ValorOz de la operación; a no ser que compartan un
+  val diferente a vacio. Además para registros, deben tener en común:
+  etiquetas, número de campos y campos*/
+  /*Pos: ValorOz ligado a Val2*/
+  if (this == &val2 || get_val() == val2.get_val() && get_val() != -1){
+    /*Caso en el que se trate de ligar un ValorOz a si mismo, o a alguien
+    ligado a él, o se iguale a un ValorOz con quien comparta val y que este
+    sea diferente a vacio*/
+    //fprintf(stderr, "Unión de una variable a si misma\n");
+  }
+  else if (campos.size() > 0 && val2.campos.size() > 0 &&
+  campos.size() == val2.campos.size()){
+    /*Caso etiquetas*/
+    /*Mas de un campo e igual cantidad de campos*/
+  }
+  else if (campos.size() + val2.campos.size() == 0){
+    ValorOz *temp_father;
+    //Comprueba si ya están ligados entre si
+    if (!prev_union(this, &val2) && !prev_union(&val2, this)){
+      //Emparentamiento de izquierda a derecha
+      if (father == NULL){
+        /*Si el otro tiene father se le pasaran los hijos del ValorOz al
+        father de val2, de lo contrario se pasaran a val2*/
+        val2.get_father()?temp_father = val2.get_father(): temp_father = &val2;
+        if (get_val() != -1){
+          *(temp_father) = get_val();
+        }
+        else if (val2.get_val() != -1)
+        *(temp_father) = val2.get_val();
+
+        temp_father->add_sons(this);
+        if (sons.size()){
+          temp_father->add_sons(get_sons());
+          elim_sons();
+        }
+        /*Si val2 tiene father el ValorOz se liga al father de val2, de lo
+        contrario se ligara a val2*/
+        set_father(temp_father);
+      }
+      //Emparentamiento de derecha a izquierda
+      else if (val2.father == NULL){
+        /*Si el ValorOz tiene father se le pasaran los hijos de val2 al
+        father, de lo contrario se pasaran al ValorOz*/
+        get_father()?temp_father = get_father(): temp_father = this;
+        if (get_val() != -1)
+        *(temp_father) = get_val();
+        else if (val2.get_val() != -1)
+        *(temp_father) = val2.get_val();
+
+        temp_father->add_sons(&val2);
+        if (val2.sons.size()){
+          temp_father->add_sons(val2.get_sons());
+          val2.elim_sons();
+        }
+        /*Si el ValorOz tiene father val2 se liga al father del ValorOz, de lo
+        contrario se ligara a ValorOz*/
+        val2.set_father(temp_father);
+      }
+      else{
+        //Casos de error
+        fprintf(stderr, "Asignacion entre variables previamente ligadas\n");
+        exit(1);
+      }
+    }
+  }
+  else{
+    if (campos.size() != val2.campos.size()){
+      fprintf(stderr, "Las etiquetas tuvieron diferentes cantidades de campos\n");
+      exit(1);
+    }
+  }
+}
+
+void ValorOz::operator =(string n_etiqueta){
+  /*Se asigna un nombre a la etiqueta del ValorOz*/
+  /*Pre: No puede estar ligado a algo diferente a vacio*/
+  /*Pos: etiqueta = n_etiqueta*/
+  if (get_val() ==  -1){
+    etiqueta = n_etiqueta;
+  }
+  else{
+    fprintf(stderr, "Asignacion de etiqueta a un ValorOz con valor ya definido\n");
+    exit(1);
   }
 }
--- ValorOz.h
+++ ValorOz.h
@@ -1,39 +1,52 @@
 #ifndef VALOROZ_H
 #define VALOROZ_H
+#include <vector>
 #include <list>
+#include <string>
+#include <map>
 
 using namespace std;
 
 class ValorOz{
   private:
     int val; //Un registro con campos no puede tener valor
-    list<ValorOz *> campos;
+    string etiqueta;
+    map<string, ValorOz *> campos;
     ValorOz *father;  /*Una variable solo puede tener un padre*/
     list<ValorOz *> sons;  /*Una variable solo puede tener multiples hijos+
-                                en caso de ser una etiqueta*/
+    en caso de ser una etiqueta*/
+    //list<ValorOz *> campos;
   public:
     ValorOz(){
       val = -1;   /*Asignar un valor inicial que equivalga a vacio,
                     que nunca ocurra*/
+      etiqueta = "";
       father = NULL;
 
     }
-    int get_val();
-    ValorOz* get_father();
-    list<ValorOz *> get_sons();
-
     void set_father(ValorOz *n_father);
     void add_sons(ValorOz *n_son);
     void add_sons(list<ValorOz *> n_sons);
     void elim_sons();
+    void set_campo(string llave, ValorOz *n_ValorOz);
+    void elim_campo(string llave);
+
+    int get_val();
+    string get_etiqueta();
+    ValorOz* get_father();
+    list<ValorOz *> get_sons();
+    list<string> get_campos_llaves();
+    ValorOz* get_campo_valor(string llave);
 
     void operator =(char v);
     void operator =(int num);
     void operator =(ValorOz &val2);
+    void operator =(string n_etiqueta);
 
     /*Pendiendtes*/
     /*Obtener cadena valor, obtener etiqueta, obtener lista de campos*/
 };
+typedef pair<string, ValorOz *> cam;
 
 
 class Almacen{
@@ -41,12 +54,12 @@ class Almacen{
     list<ValorOz *> variables;
   public:
     Almacen();/*crear almacen*/
-    void imprimir_almacen();
-    int consultar_variable(string variable);
-    bool consultar__ligadura_variable(string variable);
     void agregar_variable(string variable);
-    list<ValorOz *> obtener_lista_variables();
     int unificar_variables(string val1, string val2);
+    int consultar_variable(string variable);
+    void imprimir_almacen();
+    bool consultar__ligadura_variable(string variable);
+    list<ValorOz *> obtener_lista_variables();
 };
 
 #endif
